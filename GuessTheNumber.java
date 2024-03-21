7df366e62ceacb69a8c3f5abb14cb7c7a4d06d47
Nombulelo Dlamini <133156734+NombuleloDlamini@users.noreply.github.com>
2024-03-21 12:58:27 +0200

    Update Task1.java

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-class GuessTheNumber{
+class Task1{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
