de7eada7c1c1671c5fc3570cb2a9c52308af4f00
Nombulelo Dlamini <133156734+NombuleloDlamini@users.noreply.github.com>
2024-03-21 12:42:50 +0200

    Update GuessTheNumber.java

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,48 +1,48 @@
-import java.util.*;
-import java.io.*;
-//import java.Random;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.Random;
 
-public class GuessTheNumber
-{
-   public static void main(String[] args)
-   {
-      Scanner scanner = new Scanner(System.in);
-      Random random = new Random();
-      int min = 1;
-      int max = 100;
-      int round = 0;
-      int attempt = 0;
-      
-      while(true)
-      {
-         round++;
-         System.out.println("Round: "+round);
-         int random_number = random.nextInt(max);
-         System.out.println("Guess a random number between "+min+" and "+max);
-         
-         while(true)
-         {
-         int guess = scanner.nextInt();
-         attempt++;
-         if(guess == random_number){
-            break;
-                     }
-         else if(guess < random_number){
-         System.out.println("too low, try again:");
-         }
-         else {
-         System.out.println("too high, try again:");
-         }
-         }
-         
-         System.out.println("Yaay, you guessed correct!!");
-         System.out.println("attempts: "+attempt);
-         System.out.println("wanna play again?");
-         String ans = scanner.next();
-         
-         if(!ans.equalsIgnoreCase("yes")){
-         break;
-         } 
-      }
-   }
+class GuessTheNumber{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int min = 1;
+        int max = 100;
+        int attempts = 0;
+        int rounds = 0;
+
+        while(true){
+            rounds ++;
+            System.out.println("Round: "+rounds);
+            int randomNum = random.nextInt(max);
+
+            System.out.println("guess a random number between 1 & 100:");
+
+           while (true){
+               int guess = scanner.nextInt();
+               attempts ++;
+               if(guess==randomNum){
+                   System.out.println("you guessed the correct number, you win!!");
+                   System.out.println("attemps taken: "+attempts);
+                   attempts=0;
+                   break;
+               }
+               else if(guess<randomNum){
+                   System.out.println("too low, try again:");
+               } else {
+                   System.out.println("too high, try again:");
+               }
+
+               }
+
+            System.out.println("wanna play again?(yes/no):");
+            String answer = scanner.next();
+
+            if (!answer.equalsIgnoreCase("yes")){
+                break;
+           }
+        }
+        System.out.println("rounds played: "+rounds);
+    }
 }
